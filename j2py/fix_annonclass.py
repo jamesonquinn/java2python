98f4f7f20b637a35288b02b6dda497b201556e54
Jameson.Quinn <Jameson.Quinn+pyjamas@gmail.com>
2010-10-19 17:38:29 -0500

    anonymous classes defined before use

--- j2py/fix_annonclass.py
+++ j2py/fix_annonclass.py
@@ -3,15 +3,44 @@ import aterm
 """
 handle annonymous classes
 """
+anon_class_name = "__AnonClass__"
+class_dec = '''
+ClassDec(
+  ClassDecHead(
+    [Public(), Final()]
+    , Id("'''+anon_class_name+'''")
+    , None()
+    , None()
+    , None()
+  )
+  , ClassBody())
+'''
+    
+
+    
+can_add_classdec = ("Block",)
 
 @aterm.transformation
 def fix_annonclass(ast):
     for ni in ast.findall('NewInstance'):
-        parents = [p for p in ni.parents()]
-        print parents
-        
-        import sys
-        sys.exit()
+        ##print "QQQQ", ni[3]
+        if ni[3].name == "Some":
+          
+            for p in ni.parents():
+                if p.name in can_add_classdec:
+                    #add declaration for anon class
+                    cblock = aterm.decode(class_dec)
+                    cblock[1].extend(ni[3][0].copy())
+                    p[0].insert(0,cblock)
+                    
+                    #remove inline declaration
+                    while True:
+                      try:
+                        ni.pop()
+                      except:
+                        break
+                    ni.name = "Id"
+                    ni.append(anon_class_name)
         
     return ast
 
