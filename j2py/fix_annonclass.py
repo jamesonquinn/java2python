4c24eda538f20bb375d30cb4c7a336beda601dd4
Jameson.Quinn <Jameson.Quinn+pyjamas@gmail.com>
2010-10-21 09:48:29 -0500

    further style fixes to "fix_annonclass"

--- j2py/fix_annonclass.py
+++ j2py/fix_annonclass.py
@@ -27,24 +27,18 @@ class_make_instance = '''NewInstance(
     
 can_add_classdec = ("Block",)
 
-def MemoryIter(seed, iter):
-    b = seed
-    for i in iter:
-        a, b = b, i
-        yield a, b
-
 @aterm.transformation
 def fix_annonclass(ast):
     for ni in ast.findall('NewInstance'):
         ##print "QQQQ", ni[3]
         if ni[3].name == "Some":
-          
-            for pc, p in ni.parents():
+            parents = [ni] + [p for p in ni.parents()]
+            for p in parents:
                 if p.name in can_add_classdec:
                     #add declaration for anon class
                     cblock = aterm.decode(class_dec)
                     cblock[1].extend(ni[3][0].copy())
-                    pc.add_before(cblock)
+                    parents[parents.index(p) - 2].add_before(cblock)
                     
                     #remove inline declaration
                       
